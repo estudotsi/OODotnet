6585c7844363da5b9d785e9c3beb41a247fb030e
Rogerio Martins Campos Martins Campos <zzmelzz@hotmail.com>
2023-04-26 14:27:45 -0300

    2604

---
+++ C#OO/ContaCorrente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_OO
+{
+	public class ContaCorrente
+	{
+        public int Agencia { get; set; }
+        public string Conta { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor) 
+        {
+            Saldo -= valor;
+        }
+    }
+}
---
+++ C#OO/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C_OO;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.Titular = "Andre";
+contaDoAndre.Agencia = 15;
+contaDoAndre.Conta = "1010-x";
+contaDoAndre.Saldo = 100;
+
+//Console.WriteLine("Objeto" + contaDoAndre);
+//Console.WriteLine("Saldo" + contaDoAndre.Saldo);
+
+Console.WriteLine(contaDoAndre.Saldo);
+contaDoAndre.Depositar(80);
+contaDoAndre.Sacar(30);
+Console.WriteLine(contaDoAndre.Saldo);
